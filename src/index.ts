6d0fdee956874f7f229e9b1b10415d15638639c3
Mki Pokryvailo <mpokryva@gmail.com>
2024-07-22 10:22:12 -0400

    prettier and add more auth stuff

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,47 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-// import type { components } from './model';
-import expressBasicAuth from 'express-basic-auth';
+import type { components } from './model';
+import basicAuth, { IBasicAuthedRequest } from 'express-basic-auth';
 
-// type ErrorResponse = components["schemas"]["Error"];
-
-
+type ErrorResponse = components['schemas']['Error'];
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 // TODO: Remove eventually
 db.serialize(() => {
-  db.run("CREATE TABLE lorem (info TEXT)");
+  db.run('CREATE TABLE lorem (info TEXT)');
 
-  const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
   for (let i = 0; i < 10; i++) {
-    stmt.run("Ipsum " + i);
+    stmt.run('Ipsum ' + i);
   }
   stmt.finalize();
-
 });
-
 
 // TODO: Clean up (reorganize)
 const app = express();
 app.use(bodyParser.json());
-const apiKeyAuthMiddleware = expressBasicAuth({authorizer: apiKeyAuthorizer});
-app.use((req, res, next) => shouldAuthenticate(req) ? apiKeyAuthMiddleware(req, res, next) : next());
+const apiKeyAuthMiddleware = basicAuth({
+  authorizer: apiKeyAuthorizer,
+  unauthorizedResponse: unauthorizedResponse,
+});
+app.use(
+  (req, res, next) => {
+    shouldAuthenticate(req) ? apiKeyAuthMiddleware(req, res, next) : next();
+  },
+  (_req: basicAuth.IBasicAuthedRequest, _res, next) => {
+    next();
+  },
+);
 const port = 3000;
 
 function shouldAuthenticate(req: Request): boolean {
- return (!(req.path === "/api_keys" && req.method === "POST"));
+  return !(req.path === '/api_keys' && req.method === 'POST');
+}
+
+function unauthorizedResponse(_: Request): ErrorResponse {
+  return { message: 'Must provide a valid API key' };
 }
 
 function apiKeyAuthorizer(user: string, _: string): boolean {
@@ -39,46 +49,44 @@ function apiKeyAuthorizer(user: string, _: string): boolean {
 }
 
 app.get('/', (_: Request, res: Response) => {
-  db.all("SELECT rowid AS id, info FROM lorem", (_, rows) => {
+  db.all('SELECT rowid AS id, info FROM lorem', (_, rows) => {
     if (rows) {
       res.send(rows);
     } else {
-      res.send("No data!");
+      res.send('No data!');
     }
   });
 });
 
 const apiKeys = new Set<String>();
 let apiKeyCounter = 0;
-app.post("/api_keys", (_: Request, res: Response) => {
+app.post('/api_keys', (_: Request, res: Response) => {
   let newApiKey = `api_key_${apiKeyCounter}`;
   apiKeys.add(newApiKey);
   // TODO: Race condition?
   apiKeyCounter++;
   res.send({
-    key: newApiKey
+    key: newApiKey,
   });
 });
 
-app.post("/query", (req: Request, res: Response) => {
+app.post('/query', (req: IBasicAuthedRequest, res: Response) => {
+  console.log(`Auth is ${JSON.stringify(req.auth)}`);
   const query = req.body.query;
   if (!query) {
-    res.send({"error": "must provide query"});
+    res.send({ error: 'must provide query' });
   } else {
     db.all(query, (err, rows) => {
       if (err) {
         res.status(400).send(err);
         return;
       } else {
-       res.send({data: rows});
+        res.send({ data: rows });
       }
     });
   }
 });
 
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-
--- src/model.d.ts
+++ src/model.d.ts
@@ -4,143 +4,149 @@
  */
 
 export interface paths {
-    "/policies": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get?: never;
-        put?: never;
-        /** Create an authorization policy for the given API key */
-        post: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: never;
-        };
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
+  '/policies': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
     };
-    "/query": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
+    get?: never;
+    put?: never;
+    /** Create an authorization policy for the given API key */
+    post: {
+      parameters: {
+        query?: never;
+        header?: never;
+        path?: never;
+        cookie?: never;
+      };
+      requestBody: {
+        content: {
+          '*/*'?: never;
         };
-        get?: never;
-        put?: never;
-        /** Execute a SQLite query */
-        post: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody: {
-                content: {
-                    "application/json": {
-                        query?: string;
-                    };
-                };
-            };
-            responses: {
-                /** @description The results of the query */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content?: never;
-                };
-                /** @description Unauthenticated. Missing a valid API key. */
-                401: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["Error"];
-                    };
-                };
-                /** @description Unauthorized to execute the given query */
-                403: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["Error"];
-                    };
-                };
-            };
-        };
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
+      };
+      responses: never;
     };
-    "/api_keys": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get?: never;
-        put?: never;
-        /** Create an API key */
-        post: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                201: components["responses"]["APIKeyResponse"];
-            };
-        };
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
+  '/query': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
     };
+    get?: never;
+    put?: never;
+    /** Execute a SQLite query */
+    post: {
+      parameters: {
+        query?: never;
+        header?: never;
+        path?: never;
+        cookie?: never;
+      };
+      requestBody: {
+        content: {
+          'application/json': {
+            query?: string;
+          };
+        };
+      };
+      responses: {
+        /** @description The results of the query */
+        200: {
+          headers: {
+            [name: string]: unknown;
+          };
+          content: {
+            'application/json': Record<string, never>;
+          };
+        };
+        /** @description Unauthenticated. Missing a valid API key. */
+        401: {
+          headers: {
+            [name: string]: unknown;
+          };
+          content: {
+            'application/json': components['schemas']['Error'];
+          };
+        };
+        /** @description Unauthorized to execute the given query */
+        403: {
+          headers: {
+            [name: string]: unknown;
+          };
+          content: {
+            'application/json': components['schemas']['Error'];
+          };
+        };
+      };
+    };
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
+  '/api_keys': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get?: never;
+    put?: never;
+    /** Create an API key */
+    post: {
+      parameters: {
+        query?: never;
+        header?: never;
+        path?: never;
+        cookie?: never;
+      };
+      requestBody?: never;
+      responses: {
+        201: components['responses']['APIKeyResponse'];
+      };
+    };
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
 }
 export type webhooks = Record<string, never>;
 export interface components {
-    schemas: {
-        Error: {
-            message?: string;
-        };
+  schemas: {
+    Error: {
+      message?: string;
     };
-    responses: {
-        /** @description A new API key */
-        APIKeyResponse: {
-            headers: {
-                [name: string]: unknown;
-            };
-            content: {
-                "application/json": {
-                    api_key?: string;
-                };
-            };
+  };
+  responses: {
+    /** @description A new API key */
+    APIKeyResponse: {
+      headers: {
+        [name: string]: unknown;
+      };
+      content: {
+        'application/json': {
+          api_key?: string;
         };
+      };
     };
-    parameters: never;
-    requestBodies: never;
-    headers: never;
-    pathItems: never;
+  };
+  parameters: never;
+  requestBodies: never;
+  headers: never;
+  pathItems: never;
 }
 export type $defs = Record<string, never>;
 export type operations = Record<string, never>;
